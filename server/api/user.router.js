d557e68a3996b1310f6c59d72db6881c13392298
Finn Terdal <fterdal@gmail.com>
2017-09-05 12:05:48 -0500

    Skeleton of api and db configurations

--- server/api/user.router.js
+++ server/api/user.router.js
@@ -1,11 +1,9 @@
 const router = require('express').Router();
 
-const users = require('./user.router.js');
-router.use('/users', users);
-
 // Dummy route
 router.get('/', (req, res, next) => {
-  res.send('Hello from the API index!');
+  res.send('Hello from the users API!');
 })
+
 
 module.exports = router;

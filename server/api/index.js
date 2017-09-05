c6feb189c541b4d765c2f2168b9b3f0992314b0c
Finn Terdal <fterdal@gmail.com>
2017-09-05 11:28:19 -0500

    Set up static routing

--- server/api/index.js
+++ server/api/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 
 // Dummy route
 router.get('/', (req, res, next) => {
-  res.send('Hello, World!');
+  res.send('Hello from the API!');
 })
 
 module.exports = router;
--- server/index.js
+++ server/index.js
@@ -1,13 +1,20 @@
 const morgan = require('morgan');
-const app = require('express')();
+const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path')
 const apiRoutes = require('./api');
 
+const app = express();
+
+// Use morgan for logging
 app.use(morgan('dev'));
 
 // Body parsing middleware.
-app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Static files come from the /public directory
+app.use(express.static(path.resolve(__dirname, '..', 'public')))
 
 // Mount the api
 app.use('/api', apiRoutes);
